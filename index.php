cfe88f6f755891950282816aee44e82e7f987747
biggdred <allijgibs@gmail.com>
2017-04-11 10:56:10 +0300

    beginer

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Создание лендинга на заказ</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="Заказ на создание лендинга">
+	<meta name="keywords" content="заказ лендинга, создание лендинга">
+	<link rel="short icon" type="image/x-icon" href="/favicon.ico">
+	<link type="text/css" rel="stylesheet" href="/styles/style.css">
+	<script type="text/javascript" src="js/jquery-3.2.0.min.js"></script>
+	<script type="text/javascript" src="js/function.js"></script>
+
+</head>
+<body>
+
+</body>
+</html>
